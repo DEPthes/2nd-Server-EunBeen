52c3574631b5c61a5c5b85cdcf1732f74b16bf3a
EunbeenDev <ros4312@naver.com>
2023-06-08 03:53:00 +0900

    add SpringConfig(MemberRepository) code

--- src/main/java/DEPth_SpringBasic/DEPth_SpringBasic/SpringConfig.java
+++ src/main/java/DEPth_SpringBasic/DEPth_SpringBasic/SpringConfig.java
@@ -2,19 +2,18 @@ package DEPth_SpringBasic.DEPth_SpringBasic;
 
 import DEPth_SpringBasic.DEPth_SpringBasic.Service.MemberService;
 import DEPth_SpringBasic.DEPth_SpringBasic.repository.MemberRepository;
-import DEPth_SpringBasic.DEPth_SpringBasic.repository.MemoryMemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
-public class SpringConfig {
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
-    } //memberService 스프링 빈에 등록
 
+public class SpringConfig {
+    private final MemberRepository memberRepository;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
 }
+//스프링 데이터 JPA가 SpringDataJpaMemberRepository 를 스프링 빈으로 자동 등록
