476ec5016f6bed7422fae04ce050db974c0e3272
EunbeenDev <ros4312@naver.com>
2023-06-08 03:50:47 +0900

    add comment

--- src/main/java/DEPth_SpringBasic/DEPth_SpringBasic/repository/MemberRepository.java
+++ src/main/java/DEPth_SpringBasic/DEPth_SpringBasic/repository/MemberRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
+    //save, findById, findByName, findAll 구현
     Member save(Member member);
     Optional<Member> findById(Long id);
     Optional<Member> findByName(String name);
