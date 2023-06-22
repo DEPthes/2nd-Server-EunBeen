b4e914af7836700da7157ed5a27fa8178452f607
EunbeenDev <ros4312@naver.com>
2023-06-23 00:17:32 +0900

    add dir

--- src/main/java/DEPth_SpringBasic/DEPth_SpringBasic/DEPthSpringBasicApplication.java
+++ src/main/java/DEPth_SpringBasic/DEPth_SpringBasic/DEPthSpringBasicApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DePthSpringBasicApplication {
+public class DEPthSpringBasicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DePthSpringBasicApplication.class, args);
+		SpringApplication.run(DEPthSpringBasicApplication.class, args);
 	}
 
 }
