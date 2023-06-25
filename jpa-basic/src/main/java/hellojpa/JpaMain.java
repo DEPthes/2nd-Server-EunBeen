f89ee1b47e2c03be7b91cfcd3dd16864c92c305b
EunbeenDev <ros4312@naver.com>
2023-06-26 04:36:31 +0900

    edit Member CRUD code

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -10,7 +10,7 @@ public class JpaMain {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em=emf.createEntityManager();
+        EntityManager em = emf.createEntityManager();
 
         //트랜잭션 단위로 처리
         EntityTransaction tx=em.getTransaction();
@@ -19,30 +19,46 @@ public class JpaMain {
 
 
         try{
-            //회원 등록
-//            Member member=new Member();
-//
-//            member.setId(2L);
-//            member.setName("HelloB");
-//
-//            //persist로 memeber 저장
-//            em.persist(member);
-//
-//            //회원 조회
-//            Member findMember = em.find(Member.class,1L);
-//
-//            //회원 수정
-//            findMember.setName("HelloJPA");
-//            //resist 불필요
-//
-//            //회원 삭제
-//            em.remove(findMember);
 
-            //JPQL
-            List<Member> result=em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList(); ////5번부터 8개를 get.
+            //회원 등록
+            //Member 객체 생성
+//            Member member=new Member();
+
+//            member.setId(1L);
+//            member.setName("강영현");
+
+//            em.persist(member); //member 저장
+
+            //All data show
+            //JPQL을 사용해 List로 결과를 받아 출력
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+
+//            for (Member member : result) {
+//                System.out.println("member.name = " + member.getName());
+//            }
+
+
+            //회원 이름 조회
+//            Member findMember = em.find(Member.class, 1L);
+
+//            System.out.println(findMember.getName());
+
+
+            //회원 수정 (resist 불필요)
+            //id=2L인 "김민식" -> "최정"으로 update
+//            Member updateMember = em.find(Member.class, 2L);
+
+//            updateMember.setName("최정");
+
+            //수정 내용 확인
+//            System.out.println("수정된 회원 이름");
+//            System.out.println(updateMember.getName());
+
+
+            //회원 삭제
+            //id=2L인 회원 정보 삭제
+            Member removeMember=em.find(Member.class, 2L);
+            em.remove(removeMember);
 
 
             //하나의 transaction이 끝남->commit
