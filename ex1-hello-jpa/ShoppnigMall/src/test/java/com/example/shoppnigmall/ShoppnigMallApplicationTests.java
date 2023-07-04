e877142810e9e277f14c040dc5126b04f3a7eba5
EunBeen Noh <101248968+EunbeenDev@users.noreply.github.com>
2023-07-05 02:42:57 +0900

    Add files via upload

---
+++ ex1-hello-jpa/ShoppnigMall/src/test/java/com/example/shoppnigmall/ShoppnigMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppnigmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppnigMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
