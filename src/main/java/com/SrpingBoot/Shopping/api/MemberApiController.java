440bcdba25d9582e42437e9e4023c032009d01c6
EunbeenDev <ros4312@naver.com>
2023-07-06 17:54:20 +0900

    add exceptions at Controller

--- src/main/java/com/SrpingBoot/Shopping/api/MemberApiController.java
+++ src/main/java/com/SrpingBoot/Shopping/api/MemberApiController.java
@@ -1,5 +1,9 @@
-package api;
+package com.SrpingBoot.Shopping.api;
 
+import com.SrpingBoot.Shopping.domain.Member;
+import com.SrpingBoot.Shopping.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,20 +13,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
+
     private  final  MemberService memberService;
 
     @PostMapping("/api/v1/members") //등록-> @PostMapping 사용
     public  CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
+        if (member.getName() == null || member.getName().isEmpty()) {
+            throw new Exceptions.InvalidRequestException("Invalid user name");
+        }
+
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
 
     @Data
     static class CreateMemberResponse{
+
         private Long id;
 
-        public CreateMemberResponse(Long id){
-            this.id=id;
+        public CreateMemberResponse(Long id) {
+            this.id = id;
         }
     }
 
