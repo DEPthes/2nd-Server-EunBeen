a196cc95b7f7861ed18600a27df351bd1ca68ec5
EunbeenDev <ros4312@naver.com>
2023-07-14 19:44:03 +0900

    [fix] annotation 수정

--- src/main/java/com/SrpingBoot/Shopping/Member/MemberRepository.java
+++ src/main/java/com/SrpingBoot/Shopping/Member/MemberRepository.java
@@ -2,9 +2,11 @@ package com.SrpingBoot.Shopping.Member;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 //JpaRepository를 상속받아 기본적인 CRUD 메서드를 상속
-//save(), findById(), findAll(), deleteById() 등의 메서드 포함된 것
-@org.springframework.stereotype.Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
+//save(), findById(), findAll(), deleteById() 등의 메서드가 포함되어 있다.
+@Repository
+public interface MemberRepository extends JpaRepository<MemberDto, Long> {
+
 }
