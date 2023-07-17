ad373bc23ebd5f331b926e88fd32ca7485fbc2d0
EunbeenDev <ros4312@naver.com>
2023-07-17 20:28:39 +0900

    [fix] MemberRepository generic type 변경
    MemberDto->Member

--- src/main/java/com/SrpingBoot/Shopping/Member/MemberRepository.java
+++ src/main/java/com/SrpingBoot/Shopping/Member/MemberRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 //JpaRepository를 상속받아 기본적인 CRUD 메서드를 상속
 //save(), findById(), findAll(), deleteById() 등의 메서드가 포함되어 있다.
 @Repository
-public interface MemberRepository extends JpaRepository<MemberDto, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
 }
