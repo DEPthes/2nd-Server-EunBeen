d1d29e199882bbf72c92f59d7a82cd4f9a6caf35
EunbeenDev <ros4312@naver.com>
2023-07-19 02:48:09 +0900

    [feat] Member Entity updateMember method 및 연관관계 추가
    객체 내 setter 대신, update 메소드 생성

--- src/main/java/com/SrpingBoot/Shopping/Member/Member.java
+++ src/main/java/com/SrpingBoot/Shopping/Member/Member.java
@@ -1,15 +1,17 @@
 package com.SrpingBoot.Shopping.Member;
 
+import com.SrpingBoot.Shopping.Order.Order;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
+import java.util.*;
 
 @Entity
-@Table(name="member")
+@Table(name="MEMBER")
 @Getter
 @Builder
 @AllArgsConstructor
@@ -33,13 +35,19 @@ public class Member {
     @Column(name="EMAIL")
     private String email;
 
-    @NotEmpty
+    @Min(0)
     @Column(name="POINT")
     private int point;
 
+    public void updateMember(String newName, String newEmail, String newPassword) {
+        this.name = newName;
+        this.email = newEmail;
+        this.password = newPassword;
+    }
 
 
-    //    @OneToMany(mappedBy="member") //연관 관계의 주인이 아님을 표시
-//    private List<Order> orders = new ArrayList<>();
+    @OneToMany(mappedBy="member_id") //연관 관계의 주인이 아님을 표시
+    private List<Order> orders = new ArrayList<>();
+
 
 }
