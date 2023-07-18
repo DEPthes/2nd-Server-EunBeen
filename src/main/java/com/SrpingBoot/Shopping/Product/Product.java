f96856321a6d82adcd6c1db3e879aea1f0210f9c
EunbeenDev <ros4312@naver.com>
2023-07-19 03:34:40 +0900

    [feat] Product Entity 추가

--- src/main/java/com/SrpingBoot/Shopping/Product/Product.java
+++ src/main/java/com/SrpingBoot/Shopping/Product/Product.java
@@ -1,4 +1,31 @@
 package com.SrpingBoot.Shopping.Product;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="PRODUCT")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+    @Id @Column(name="ID")
+    private Long id;
+
+    @Column(name="CATEGORY_CODE")
+    private Long category_code;
+
+    @Column(name="NAME")
+    private String name;
+
+    @Column(name="STOCK")
+    private Long stock;
 }
