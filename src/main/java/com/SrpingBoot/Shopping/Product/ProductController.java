54a0407679f687fa65621f839b2af7933aa823c2
EunbeenDev <ros4312@naver.com>
2023-07-19 12:42:41 +0900

    [#13][feat] products/all controller 페이징 기능 추가

--- src/main/java/com/SrpingBoot/Shopping/Product/ProductController.java
+++ src/main/java/com/SrpingBoot/Shopping/Product/ProductController.java
@@ -1,6 +1,10 @@
 package com.SrpingBoot.Shopping.Product;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -34,20 +38,25 @@ public class ProductController {
         return "products/createResult";
     }
 
-    @GetMapping("/all")
-    public String findAll(Model model) {
-        List<Product> products = productService.findAll();
-        model.addAttribute("products", products);
-        return "products/allProducts";
-    }
-
-
 
     @GetMapping("/details/{id}")
     public String getProductDetails(@PathVariable("id") Long id, Model model) {
         Optional<Product> product = productService.findById(id);
         model.addAttribute("product", product.orElse(null));
         return "products/detailResult";
+    }
+
+    @GetMapping("/all")
+    public String productList(Model model, @PageableDefault(page = 0, size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<Product> list = productService.productList(pageable);
+        int nowPage = list.getNumber() + 1; // 현재 페이지 번호
+        int startPage = Math.max(nowPage - 4, 1); // 시작 페이지 번호
+        int endPage = Math.min(nowPage + 4, list.getTotalPages()); // 끝 페이지 번호
+        model.addAttribute("products", list);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        return "products/allProducts";
     }
 
 }
