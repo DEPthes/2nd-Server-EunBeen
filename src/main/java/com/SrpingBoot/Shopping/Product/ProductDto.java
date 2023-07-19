aa94c7e91ac2fb82b8d96238fdf954a426a1ec2c
EunbeenDev <ros4312@naver.com>
2023-07-19 12:00:54 +0900

    [feat] ProductDto 작성
    Long의 경우 @NotNull 사용

--- src/main/java/com/SrpingBoot/Shopping/Product/ProductDto.java
+++ src/main/java/com/SrpingBoot/Shopping/Product/ProductDto.java
@@ -1,4 +1,48 @@
 package com.SrpingBoot.Shopping.Product;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
 public class ProductDto {
+    private Long id;
+    private Long categoryCode;
+    private String name;
+    private String desc;
+    private Long price;
+    private Long stock;
+
+    @Data
+    static class CreateProductRequest{
+        @NotNull
+        private Long categoryCode;
+        @NotEmpty
+        private String name;
+
+        private String desc;
+        @NotNull
+        private Long price;
+        @NotNull
+        private Long stock;
+
+    }
+    @Data
+    static class CreateProductResponse{
+        private Long id;
+        public CreateProductResponse(Long id) {
+            this.id = id;
+        }
+
+    }
+
+
+
+
 }
