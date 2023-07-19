04c85300ce3017d2c5cf92145ba8012a256465ac
EunbeenDev <ros4312@naver.com>
2023-07-19 12:01:56 +0900

    [#8][feat] ProductService 메소드 구현

--- src/main/java/com/SrpingBoot/Shopping/Product/ProductService.java
+++ src/main/java/com/SrpingBoot/Shopping/Product/ProductService.java
@@ -1,4 +1,37 @@
 package com.SrpingBoot.Shopping.Product;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository){this.productRepository=productRepository;}
+
+    @Transactional
+    public Long join(ProductDto.CreateProductRequest request) {
+        Product product = Product.builder()
+                .categoryCode(request.getCategoryCode())
+                .name(request.getName())
+                .desc(request.getDesc())
+                .price(request.getPrice())
+                .stock(request.getStock())
+                .build();
+
+        Product savedProduct = productRepository.save(product);
+        return savedProduct.getId();
+    }
+
+
+    public Optional<Product> findById(Long id) { return productRepository.findById(id); }
+    public List<Product> findAll() { return productRepository.findAll(); }
+
+    public void deleteById(Long id) { productRepository.deleteById(id); }
+
+    public void update(Product product) { productRepository.save(product); }
 }
+
